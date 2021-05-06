7857fe027cde2bc161f3c436fe86d678cffc5d53
Riri <riri.zamor@gmail.com>
2021-05-06 14:56:16 +0200

    crud user_lik_usl

---
+++ routes/like.user.routes.js
@@ -0,0 +1,9 @@
+const userLikesController = require('../controllers/like.user.controller.js');
+const router = require('express').Router();
+
+router.post('/', userLikesController.createUserLike);
+router.get('/',userLikesController.getAllUserLikes);
+router.patch('/:id',userLikesController.putUserLike);
+router.delete('/:id',userLikesController.deleteUserLike);
+
+module.exports = router;
