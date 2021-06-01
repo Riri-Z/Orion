ba663b73c2c6d61591980ba416eb4335d43a2f70
AnaH-1907 <anaisgagneur@gmail.com>
2021-06-01 15:34:18 +0200

    ...4

--- sql/dump.sql
+++ sql/dump.sql
@@ -234,6 +234,8 @@ CREATE TABLE IF NOT EXISTS `post_pos` (
   `pos_deletedAt` DATE NULL,
   `id_exa` INT NULL,
   `id_usr` INT NOT NULL,
+  `id_parent` INT NULL,
   PRIMARY KEY (`id_pos`),
   FOREIGN KEY (`id_exa`) REFERENCES `exam_exa` (`id_exa`),
+  FOREIGN KEY (`id_parent`) REFERENCES `post_pos` (`id_pos`),
   FOREIGN KEY (`id_usr`) REFERENCES `user_usr` (`id_usr`)) ENGINE = InnoDB;
