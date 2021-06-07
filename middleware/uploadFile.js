26c876f0c247ce94986df61308831b3043e6c0ca
Riri Z <riri.zamor@gmail.com>
2021-06-07 23:29:16 +0200

    add cloudinary upload /put user

--- middleware/uploadFile.js
+++ middleware/uploadFile.js
@@ -1,3 +1,5 @@
+const { cloudinary } = require('../utils/cloudinary')
+
 const upload = (req, res, next) => {
 
   let sampleFile;
@@ -14,4 +16,28 @@ const upload = (req, res, next) => {
   next()
 }
 
-module.exports = upload;
+const uploadResponse = async (req, res, next) => {
+  try {
+    let sampleFile;
+    let uploadPath;
+    if (!req.files || Object.keys(req.files).length === 0) {
+      return next() /* res.status(400).send('No files were uploaded') */
+    }
+    sampleFile = req.files.sampleFile
+    sampleFile.name = sampleFile.name.toLowerCase()
+    uploadPath = __dirname + '/uploads/' + sampleFile.name
+    await sampleFile.mv(uploadPath, function (err) {
+      if (err) return res.status(500).send(err)
+    })
+
+
+    await cloudinary.uploader.upload(
+      "middleware/uploads/" + req.files.sampleFile.name.toLowerCase())
+
+    next()
+  } catch (e) {
+    res.json(e)
+  }
+
+}
+module.exports = uploadResponse;
