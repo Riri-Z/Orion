8d4a4a409312ca4f72d27e82668aca8a84b3190a
Riri <riri.zamor@gmail.com>
2021-07-13 18:07:10 +0200

    Update multiple model

--- routes/answer.routes.js
+++ routes/answer.routes.js
@@ -4,6 +4,7 @@ const errorHandler = require('../controllers/error');
 
 router.post('/', errorHandler.errorHandler(Answer.create));
 router.get('/', errorHandler.errorHandler(Answer.getAll));
+router.get('/availableAnswer', errorHandler.errorHandler(Answer.getSpecific));
 router.put('/:id', errorHandler.errorHandler(Answer.put));
 router.delete('/:id', errorHandler.errorHandler(Answer.delete));
 
