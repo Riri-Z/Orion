d16211469e2e153bc73adf11463c57c67d720ec9
Riri <riri.zamor@gmail.com>
2021-07-15 20:54:45 +0200

    Update config to env

--- utils/database.js
+++ utils/database.js
@@ -3,10 +3,10 @@ const mysql = require('mysql2');
 const config = require('../config/config.json');
 
 const pool = mysql.createPool({
-    host: config.host,
-    user: config.user,
-    database: config.database,
-    password: config.password
+    host: process.env.host,
+    user: process.env.user,
+    database: process.env.database,
+    password: process.env.password,
 });
 
 module.exports = pool.promise();
