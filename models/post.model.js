124370ef6a1f1fdc8b22b1bc06a6af1b226ca103
AnaH-1907 <anaisgagneur@gmail.com>
2021-07-16 00:45:13 +0200

    last modif

--- models/post.model.js
+++ models/post.model.js
@@ -19,11 +19,11 @@ module.exports = class Post {
     }
     
     static fetchAllParent(){
-        return db.execute('SELECT id_pos, pos_content, pos_createdAt , (SELECT COUNT(*) FROM post_pos b WHERE b.id_parent = post_pos.id_pos) AS answers, (SELECT COUNT(*) FROM like_lik WHERE like_lik.id_pos = post_pos.id_pos) AS stars,(SELECT usr_firstname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_firstname, (SELECT usr_lastname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_lastname, (SELECT usr_imgURL FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS avatar FROM post_pos WHERE post_pos.id_parent IS NULL;');
+        return db.execute('SELECT id_pos, pos_content, pos_createdAt , id_usr, (SELECT COUNT(*) FROM post_pos b WHERE b.id_parent = post_pos.id_pos) AS answers, (SELECT COUNT(*) FROM like_lik WHERE like_lik.id_pos = post_pos.id_pos) AS stars,(SELECT usr_firstname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_firstname, (SELECT usr_lastname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_lastname, (SELECT usr_imgURL FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS avatar FROM post_pos WHERE post_pos.id_parent IS NULL;');
     }
 
     static fetchAllChildren(){
-        return db.execute('SELECT id_pos, pos_content, pos_createdAt, id_parent, (SELECT COUNT(*) FROM like_lik WHERE like_lik.id_pos = post_pos.id_pos) AS stars, (SELECT usr_firstname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_firstname, (SELECT usr_lastname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_lastname, (SELECT usr_imgURL FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS avatar FROM post_pos WHERE post_pos.id_parent IS NOT NULL;');
+        return db.execute('SELECT id_pos, pos_content, pos_createdAt, id_parent, id_usr, (SELECT COUNT(*) FROM like_lik WHERE like_lik.id_pos = post_pos.id_pos) AS stars, (SELECT usr_firstname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_firstname, (SELECT usr_lastname FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS usr_lastname, (SELECT usr_imgURL FROM user_usr WHERE user_usr.id_usr = post_pos.id_usr) AS avatar FROM post_pos WHERE post_pos.id_parent IS NOT NULL;');
     }
 
 
